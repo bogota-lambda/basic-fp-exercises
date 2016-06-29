d3bd8d8d13070672f196a4de2b4755fc360a7ade
miguel <miguelvilag@gmail.com>
2016-06-29 11:00:36 -0500

    renombramiento ménor

--- src/main/scala/exercises/FunctionalDataStructures.scala
+++ src/main/scala/exercises/FunctionalDataStructures.scala
@@ -18,72 +18,72 @@ object List {
   }
 
   // exercise 
-  def drop[A](l: List[A], n: Int): List[A] =
+  def drop[A](list: List[A], n: Int): List[A] =
     if(n <= 0) {
-      l
+      list
     } else {
-      l match {
+      list match {
         case Nil        => Nil
         case Cons(x,xs) => drop(xs, n-1)
       }
     }
 
   // exercise
-  def dropWhile[A](l: List[A])( p: A => Boolean): List[A] = l match {
+  def dropWhile[A](list: List[A])( p: A => Boolean): List[A] = list match {
     case Nil => Nil
     case Cons(x,xs) =>
       if(p(x))
         dropWhile(xs)(p)
       else
-        l
+        list
   }
 
-  def foldRight[A,B](l: List[A], z: B)(f: (A,B) => B): B = l match {
+  def foldRight[A,B](list: List[A], z: B)(f: (A,B) => B): B = list match {
     case Nil        => z
     case Cons(x,xs) => f( x, foldRight(xs,z)(f) )
   }
 
-  def sum2(l: List[Int]): Int = foldRight(l, 0)(_ + _)
+  def sum2(list: List[Int]): Int = foldRight(list, 0)(_ + _)
 
-  def product2(l: List[Int]): Int = foldRight(l, 1)(_ * _)
-
-  // exercise
-  def length[A](l: List[A]): Int = foldRight(l,0)( (_,acc) => acc+1 )
+  def product2(list: List[Int]): Int = foldRight(list, 1)(_ * _)
 
   // exercise
-  def foldLeft[A,B](l: List[A], z: B)(f: (B,A) => B): B = {
+  def length[A](list: List[A]): Int = foldRight(list,0)( (_,acc) => acc+1 )
+
+  // exercise
+  def foldLeft[A,B](list: List[A], z: B)(f: (B,A) => B): B = {
     @annotation.tailrec
-    def loop(l: List[A], acc: B): B = l match {
+    def loop(list: List[A], acc: B): B = list match {
       case Nil         => acc
       case Cons(x, xs) => loop(xs, f(acc,x))
     }
-    loop(l, z)
+    loop(list, z)
   }
 
   // exercise
-  def reverse[A](l: List[A]): List[A] =
-    foldLeft(l, List[A]())( (tail, x) => Cons(x,tail) )
+  def reverse[A](list: List[A]): List[A] =
+    foldLeft(list, List[A]())( (tail, x) => Cons(x,tail) )
 
   // exercise
-  def addOne2All(l: List[Int]): List[Int] = l match {
+  def addOne2All(list: List[Int]): List[Int] = list match {
     case Nil        => Nil
     case Cons(x,xs) => Cons(x+1, addOne2All(xs))
   }
 
   // exercise
-  def allDoubles2Strings(l: List[Double]): List[String] = l match {
+  def allDoubles2Strings(list: List[Double]): List[String] = list match {
     case Nil        => Nil
     case Cons(x,xs) => Cons(x.toString, allDoubles2Strings(xs))
   }
 
   // exercise
-  def map[A,B](l: List[A])(f: A => B): List[B] = l match {
+  def map[A,B](list: List[A])(f: A => B): List[B] = list match {
     case Nil        => Nil
-    case Cons(x,xs) => Cons(f(x), map(l)(f))
+    case Cons(x,xs) => Cons(f(x), map(list)(f))
   }
 
   // exercise
-  def filter[A](l: List[A])(f: A => Boolean): List[A] = foldRight(l, empty[A])( (x,xs) => if(f(x)) Cons(x,xs) else xs )
+  def filter[A](list: List[A])(f: A => Boolean): List[A] = foldRight(list, empty[A])( (x,xs) => if(f(x)) Cons(x,xs) else xs )
 
   // Constructor varíadico
   def apply[A](as: A*): List[A] =
